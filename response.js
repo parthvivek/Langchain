db38734fb499329f5c738982e958ae74be46a158
parthvivek <kumarvivekgupta1810@gmail.com>
2026-02-01 22:17:12 +0530

    added the review chain

---
+++ response.js
@@ -0,0 +1,28 @@
+{
+"1":
+{
+"no": "1",
+"question": "Question text goes here",
+"options": {
+"A": "Option 1",
+"B": "Option 2",
+"C": "Option 3",
+"D": "Option 4"
+},
+"correct_answer": "B"
+}
+
+"2":
+{
+"no": "2",
+"question": "Question text goes here",
+"options": {
+"A": "Option 1",
+"B": "Option 2",
+"C": "Option 3",
+"D": "Option 4"
+},
+"correct_answer": "B"
+}
+
+}
